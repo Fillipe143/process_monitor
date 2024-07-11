c3794d6ae6b7b37161194044d87d081f7979348e
Fillipe143 <fillipedev.contato@gmail.com>
2024-07-11 14:27:47 -0300

    added def to get list of all pids

--- main.py
+++ main.py
@@ -13,7 +13,16 @@ def get_pid_info(pid):
     info["cpu_percent"] = process.cpu_percent()
     info["threads"] = process.num_threads()
     info["creation_time"] = int(process.create_time())
-    info["path"] = process.exe()
+    #info["path"] = process.exe()
     info["owner"] = process.username()
 
     return info
+
+def get_process_list():
+    pids = psutil.pids()
+    process_list = []
+
+    for pid in pids:
+        process_list.append(get_pid_info(pid))
+
+    return process_list
